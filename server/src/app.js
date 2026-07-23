098954cb898e064da74fd2ae2bac3b06d2e1076d
venkata-koushik <koushik.chinu.2007@gmail.com>
2026-07-23 17:42:24 +0530

    Prepare project for deployment

--- server/src/app.js
+++ server/src/app.js
@@ -85,7 +85,7 @@ Review.belongsTo(Student, {
 
 const app = express();
 
-const clientOrigins = (process.env.CLIENT_ORIGIN || "http://localhost:5173")
+const clientOrigins = (process.env.CLIENT_ORIGIN || "http://localhost:5174")
   .split(",")
   .map((origin) => origin.trim())
   .filter(Boolean);
